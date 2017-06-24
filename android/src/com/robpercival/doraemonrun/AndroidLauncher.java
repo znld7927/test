acda0b7e77e4cabbf8eca34214575deb9fa49a13
DESKTOP-N941H3U\User <znld6715@gmail.com>
2017-06-24 17:48:59 +0900

    Initial commit

---
+++ android/src/com/robpercival/doraemonrun/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.robpercival.doraemonrun;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.robpercival.doraemonrun.DoraemonRun;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DoraemonRun(), config);
+	}
+}
