eea6c8fca0e059370cefd737c6fbc4d886c2557d
DESKTOP-N941H3U\User <znld6715@gmail.com>
2017-06-24 23:59:11 +0900

    2. 도라에몽의 날개 방향 바꿈 3. 누르면 올라갔다가 내려가는 모션 4. 끝까지 떨어지지 않고 바닥에 멈췄다가 누르면 다시 올라오는 도라에몽

--- core/src/com/robpercival/doraemonrun/DoraemonRun.java
+++ core/src/com/robpercival/doraemonrun/DoraemonRun.java
@@ -9,25 +9,52 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class DoraemonRun extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
-	Texture doraemon;
-
-	
+	Texture[] doraemons;
+	int flapState = 0;
+	float doraemonY = 0;
+	float velocity = 0;
+	int gameState = 0;
+	float gravity =2;
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
-		doraemon = new Texture("doraemon.png");
-
+		doraemons = new Texture[2];
+		doraemons[0] = new Texture("doraemon.png");
+		doraemons[1] = new Texture("doraemon2.png");
+		doraemonY = Gdx.graphics.getHeight() / 2 - doraemons[flapState].getHeight() / 2;
 	}
 
 	@Override
-	public void render () {
+	public void render() {
+
+		if (gameState != 0) {
+
+			if (Gdx.input.justTouched()) {
+				velocity = -30;
+
+			}
+			if(doraemonY > 0 || velocity < 0) {
+
+				velocity = velocity + gravity;
+				doraemonY -= velocity;
+			}
+		}else{
+			if (Gdx.input.justTouched()) {
+				gameState = 1;
+			}
+		}
+		if (flapState == 0) {
+			flapState = 1;
+		} else {
+			flapState = 0;
+		}
 		batch.begin();
-		batch.draw(background,0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(doraemon,Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight()/2);
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(doraemons[flapState], Gdx.graphics.getWidth() / 2 - doraemons[flapState].getWidth() / 2, doraemonY);
 		batch.end();
-}
-	
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
